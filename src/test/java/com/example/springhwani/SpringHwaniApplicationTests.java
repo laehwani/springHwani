2f1a5cb3e3dfbc7fc0fbdff5e74073462c2ae5d8
Hwani <klh481@naver.com>
2023-09-20 16:20:30 +0900

    first commit

---
+++ src/test/java/com/example/springhwani/SpringHwaniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springhwani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHwaniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
