6cff22a027ff667e39f7d398165101f4fe8a481f
1111 <1111@naver.com>
2023-10-10 10:11:19 +0900

    10/6 페이지 넘기기

---
+++ src/main/java/com/example/springhwani/domain/Post.java
@@ -0,0 +1,12 @@
+package com.example.springhwani.domain;
+
+import lombok.Data;
+
+@Data
+public class Post {
+    private int id;
+    private String title;
+    private String content;
+}
+
+
---
+++ src/main/java/com/example/springhwani/domain/PostService.java
@@ -0,0 +1,18 @@
+package com.example.springhwani.domain;
+
+public class PostService {
+    // 게시글 작성
+    public void createPost(Post post) {
+        // 게시글을 저장하는 로직 구현
+    }
+
+    // 게시글 수정
+    public void updatePost(Post post) {
+        // 게시글을 수정하는 로직 구현
+    }
+
+    // 게시글 삭제
+    public void deletePost(int postId) {
+        // 게시글을 삭제하는 로직 구현
+    }
+}
